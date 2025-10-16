26432efa261ac4a04f8644f3b7707b5d84554a3f
Brian Wifanie <brianwifanie@gmail.com>
2025-10-16 14:59:38 +1100

    Implemented feature generation on terrain

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,8 @@ import {
   SelectValue,
 } from "~/components/ui/select";
 import { Button } from "~/components/ui/button";
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import DiamondIcon from '@mui/icons-material/Diamond';
 
 const NUM_ROWS = 11;
 const DEFAULT_COLOUR = "#FFFFFF"
@@ -24,12 +26,42 @@ const TERRAIN_COLOURS = {
   Ocean: "#7B84DB",
   Snow: "#F0FFFE"
 }
-
-type Coords = { x: number, y: number };
+const FEATURES = {
+  Mountain: {
+    color: "#6D6A6A",
+    spawnsOn: ["Desert", "Grass", "Snow"], 
+    appearance: {
+      Desert: <LandscapeIcon className="text-[#6D6A6A]" />,
+      Grass: <LandscapeIcon className="text-[#6D6A6A]" />,
+      Snow: <LandscapeIcon className="text-[#6D6A6A]" />
+    },
+    minPerBiome: 2,
+    maxPerBiome: 6
+  },
+  Diamond: {
+    color: "#1E6182",
+    spawnsOn: ["Desert", "Grass"], 
+    appearance: {
+      Desert: <DiamondIcon className="text-[#1E6182]" />,
+      Grass: <DiamondIcon />,
+    },
+    minPerBiome: 8,
+    maxPerBiome: 12
+  },
+  Volcano: {
+    color: "#F85353",
+    spawnsOn: ["Ocean"], 
+    appearance: {
+      Ocean: <div className="w-[90%] h-[90%] rounded-full border-5 border-[#F85353]" />
+    },
+    minPerBiome: 1,
+    maxPerBiome: 10
+  }
+}
 
 export default function Home() {
   const [boardState, setBoardState] = useState(Array.from({ length: NUM_ROWS }, () => {
-    return Array(NUM_ROWS).fill(DEFAULT_COLOUR);
+    return Array.from({ length: NUM_ROWS }, () => ({ features: [], color: DEFAULT_COLOUR }));
   }));
   const [isLandscape, setIsLandscape] = useState(false);
   
@@ -42,96 +74,137 @@ export default function Home() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const updateCell = (x: number, y: number, colour: string) => {
+  const updateCell = (x, y, colour) => {
    setBoardState((prev) => {
-      const newBoard = prev.map((row) => [...row]);
-      newBoard[y]![x] = newBoard[y]![x] === colour ? DEFAULT_COLOUR : colour;
+      const newBoard = prev.map((row, rowIndex) => 
+          rowIndex === y ? row.map(cell => ({ ...cell })) : row.slice()
+      );
+      newBoard[y][x] = {
+          ...newBoard[y][x],
+          color: newBoard[y][x].color === colour ? DEFAULT_COLOUR : colour
+      };
       return newBoard;
     });
   }
 
-  const getValidNeighbours = (x: number, y: number) => {
+  const getValidNeighbours = (x, y) => {
     const validNeighbours = [];
-
+  
     if ((x + 1) < NUM_ROWS) validNeighbours.push({ x: x + 1, y });
     if ((x - 1) >= 0) validNeighbours.push({ x: x - 1, y });
     if ((y + 1) < NUM_ROWS) validNeighbours.push({ x, y: y + 1 });
     if ((y - 1) >= 0) validNeighbours.push({ x, y: y - 1 });
-
+  
     return validNeighbours;
   }
-
+  
   const generateTerrain = () => {
     const newBoard = Array.from({ length: NUM_ROWS }, () => {
-      return Array(NUM_ROWS).fill(TERRAIN_COLOURS.Ocean);
+      return Array.from({ length: NUM_ROWS }, () => ({ features: [], color: TERRAIN_COLOURS.Ocean }));
     });
-
+  
     const usedSeedCoordinates = new Set();
     const biomes = Object.keys(TERRAIN_COLOURS)
       .filter((terrain) => terrain !== "Water" && terrain !== "Ocean")
       .map((biome) => {
       return { 
-        colour: TERRAIN_COLOURS[biome as keyof typeof TERRAIN_COLOURS], 
+        terrain: biome,
+        colour: TERRAIN_COLOURS[biome], 
         size: Math.floor(Math.random() * 16) + 30, 
-        positions: [] as Coords[]
+        positions: []
       };
     });
-
+  
     biomes.forEach((biome) => {
       while (biome.positions.length < 1) {
         const x = Math.floor(Math.random() * NUM_ROWS);
         const y = Math.floor(Math.random() * NUM_ROWS);
         const key = `${x}, ${y}`;
-
+  
         if (!usedSeedCoordinates.has(key)) {
           usedSeedCoordinates.add(key);
-          newBoard[y]![x] = biome.colour;
+          newBoard[y][x] = { ...newBoard[y][x], color: biome.colour };
           biome.positions.push({ x, y });
           biome.size--;
         }
       }
     })
-
+  
     while (biomes.some(biome => biome.size > 0)) {
       for (let i = 0; i < biomes.length; i++) {
         const biome = biomes[i];
         if (!biome) continue;
         if (biome.size <= 0) continue;
-
+  
         biome.size--;
         const initPos = biome.positions[Math.floor(Math.random() * biome.positions.length)];
         if (!initPos) continue;
         let validNeighbours = getValidNeighbours(initPos.x, initPos.y);
         validNeighbours = validNeighbours.filter((neighbour) => {
-          return newBoard[neighbour.y]![neighbour.x] === TERRAIN_COLOURS.Ocean; 
+          return newBoard[neighbour.y][neighbour.x].color === TERRAIN_COLOURS.Ocean; 
         });
         const newCell = validNeighbours[Math.floor(Math.random() * validNeighbours.length)];
         if (!newCell) continue;
-
-        newBoard[newCell.y]![newCell.x] = biome.colour;
+  
+        newBoard[newCell.y][newCell.x] = { ...newBoard[newCell.y][newCell.x], color: biome.colour };
         biome.positions.push({ x: newCell.x, y: newCell.y });
       }
     }
 
+    biomes.push({ 
+      terrain: "Water",
+      colour: TERRAIN_COLOURS["Water"], 
+      size: 0, 
+      positions: []
+    }, { 
+      terrain: "Ocean",
+      colour: TERRAIN_COLOURS["Ocean"], 
+      size: 0, 
+      positions: []
+    });
+  
     for (let y = 0; y < NUM_ROWS; y++) {
       for (let x = 0; x < NUM_ROWS; x++) {
-        if (newBoard[y]![x] === TERRAIN_COLOURS.Ocean && 
-          getValidNeighbours(x, y)
+        if (newBoard[y][x].color === TERRAIN_COLOURS.Ocean) {
+
+          if (getValidNeighbours(x, y)
             .some(
               ({ x, y }) => 
-            newBoard[y]![x] !== TERRAIN_COLOURS.Ocean && newBoard[y]![x] !== TERRAIN_COLOURS.Water
-        )) {
-          newBoard[y]![x] = TERRAIN_COLOURS.Water;
+            newBoard[y][x].color !== TERRAIN_COLOURS.Ocean && newBoard[y][x].color !== TERRAIN_COLOURS.Water
+          )) {
+            newBoard[y][x] = { ...newBoard[y][x], color: TERRAIN_COLOURS.Water };
+            const waterBiome = biomes.find(biome => biome.terrain === "Water");
+            waterBiome.positions.push({ x, y });
+          } else {
+            const oceanBiome = biomes.find(biome => biome.terrain === "Ocean");
+            oceanBiome.positions.push({ x, y });
+          }
         }
       }
     }
 
+    console.log(biomes);
+
+
+    Object.values(FEATURES).forEach((feature) => {
+      feature.spawnsOn.forEach((validBiome) => {
+        let numFeatures = Math.floor(Math.random() * (feature.maxPerBiome - feature.minPerBiome + 1)) + feature.minPerBiome;
+
+        while (numFeatures > 0) {
+          const biome = biomes.find(biome => biome.terrain === validBiome);
+          const { x, y } = biome.positions[Math.floor(Math.random() * biome.positions.length)];
+          newBoard[y][x] = { ...newBoard[y][x], features: [Object.keys(FEATURES).find(key => FEATURES[key] === feature)] };
+          numFeatures--;
+        }
+      })
+    })
+  
     setBoardState(newBoard);
   }
 
   const clearBoard = () => {
     const newBoard = Array.from({ length: NUM_ROWS }, () => 
-        Array(NUM_ROWS).fill(DEFAULT_COLOUR)
+      Array.from({ length: NUM_ROWS }, () => ({ features: [], color: DEFAULT_COLOUR }))
     );
     setBoardState(newBoard);
   }
@@ -153,28 +226,33 @@ export default function Home() {
         {[...Array(NUM_ROWS * NUM_ROWS)].map((_, i) => {
           const x = i % NUM_ROWS;
           const y = Math.floor(i / NUM_ROWS); 
-          const currColor = boardState[y]![x];
+          const currColor = boardState[y][x].color;
+          const currFeatures = boardState[y][x].features;
           const currTerrain = Object.keys(TERRAIN_COLOURS).find(
-            key => TERRAIN_COLOURS[key as keyof typeof TERRAIN_COLOURS] === currColor
+            key => TERRAIN_COLOURS[key] === currColor
           ) || "empty";
 
           return (
             <Popover key={i}>
               <PopoverTrigger>
                 <div
-                  className="aspect-square border border-[#000000] cursor-pointer hover:border-2" 
+                  className="aspect-square border border-[#000000] cursor-pointer hover:border-2 flex justify-center items-center" 
                   style={{ 
                     height: isLandscape ? `calc(100vh/${NUM_ROWS})` : "auto",
                     width: isLandscape ? "auto" : `calc(100vw/${NUM_ROWS})`,
                     backgroundColor: currColor
                   }} 
-                />
+                >
+                  {currFeatures.map((feature) => {
+                    return FEATURES[feature].appearance[currTerrain];
+                  })}
+                </div>
               </PopoverTrigger>
               <PopoverContent>
                 <Select 
                   value={currTerrain} 
                   onValueChange={(terrain) => {
-                    const newColor = TERRAIN_COLOURS[terrain as keyof typeof TERRAIN_COLOURS] || DEFAULT_COLOUR;
+                    const newColor = TERRAIN_COLOURS[terrain] || DEFAULT_COLOUR;
                     updateCell(x, y, newColor);
                   }}
                 >
